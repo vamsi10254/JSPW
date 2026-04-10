844f2935243bb5e32897c99b3fda17dc07e781db
vamsy <helen123@gmail.com>
2026-04-10 20:53:53 +0530

    Updated

---
+++ Javascirpt_Basics_Practice/08_Comparison operators.js
@@ -0,0 +1,9 @@
+// Comparison operators
+
+let x = 10;
+let y = "10";
+
+console.log(x == y);   // true (value only)
+console.log(x === y);  // false (value + type)
+console.log(x != y);   // false
+console.log(x > 5);    // true
